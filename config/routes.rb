de397a2a03eec9a47997f56f8301e3b7417bb759
zhaozongchang <xingyingsuifeng@gmail.com>
2017-10-07 09:01:10 +0800

    better welcome/index & admin/jobs/index

--- config/routes.rb
+++ config/routes.rb
@@ -13,7 +13,7 @@ Rails.application.routes.draw do
       resources :resumes
     end
   end
-  root 'jobs#index'
+  root 'welcome#index'
 
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
